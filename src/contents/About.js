ed8ef3b6f1980b7b6705b72b7b51476f4960ead0
PC <abdullahiyosef@gmail.com>
2020-02-22 16:41:31 +0000

    connected netlify

--- src/contents/About.js
+++ src/contents/About.js
@@ -13,7 +13,7 @@ class About extends Component {
             <p>Upon completing my Access to Higher Education in Engineering. I focused on developing my eCommerce business and decided to take a break before moving onto university. I've always been interested in learning to programming but never took the leap of faith to learn. I decided to take on the #100DaysOfCode Challenge because I regretted not knowing how to code. My goal was to code for at least 1 hour every day. I used freeCampCode to learn HTML CSS, JavaScript and I'm currently in the process of learning React. 
             </p>
             <p> 
-            II focused mainly on front-end development due to my experience in Adobe Photoshop and Illustrator. I attended local coding meetup groups to learn from more experienced programmers. Outside of learning to programme, I played basketball with friends to clear my head but often found my self thinking about coding.
+            I focused mainly on front-end development due to my experience in Adobe Photoshop and Illustrator. I attended local coding meetup groups to learn from more experienced programmers. Outside of learning to programme, I played basketball with friends to clear my head but often found my self thinking about coding.
             </p>
             
             </div>
