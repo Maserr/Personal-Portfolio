0dc7877713f37e915d2d57b01567bd3f8bd103af
PC <abdullahiyosef@gmail.com>
2020-02-23 12:28:59 +0000

    update About

--- src/contents/About.js
+++ src/contents/About.js
@@ -10,7 +10,7 @@ class About extends Component {
             <h1>I am Abdullahi Yosef</h1>
             <h3>Aspiring <u> Software Developer</u></h3>
             <br></br>
-            <p>Upon completing my Access to Higher Education in Engineering. I focused on developing my eCommerce business and decided to take a break before moving onto university. I've always been interested in learning to programming but never took the leap of faith to learn. I decided to take on the #100DaysOfCode Challenge because I regretted not knowing how to code. My goal was to code for at least 1 hour every day. I used freeCampCode to learn HTML CSS, JavaScript and I'm currently in the process of learning React. 
+            <p>Upon completing my Access to Higher Education in Engineering. I focused on developing my eCommerce business and decided to take a break before moving onto university. I've always been interested in learning to programming but never took the leap of faith to learn. I decided to take on the #100DaysOfCode Challenge because I regretted not knowing how to code. My goal was to code for at least 1 hour every day. I used freeCampCode to learn HTML CSS and used WatchandCode to learn JavaScript and I'm currently in the process of learning React. 
             </p>
             <p> 
             I focused mainly on front-end development due to my experience in Adobe Photoshop and Illustrator. I attended local coding meetup groups to learn from more experienced programmers. Outside of learning to programme, I played basketball with friends to clear my head but often found my self thinking about coding.
